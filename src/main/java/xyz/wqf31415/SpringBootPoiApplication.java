8c0256331085bd76e0c6c457ebda6b9b2f42f325
wqf31415 <wqf31415@hotmail.com>
2019-05-30 09:18:49 +0800

    feat: 修改项目启动类，增加启动时的日志输出

--- src/main/java/xyz/wqf31415/SpringBootPoiApplication.java
+++ src/main/java/xyz/wqf31415/SpringBootPoiApplication.java
@@ -1,12 +1,29 @@
 package xyz.wqf31415;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class SpringBootPoiApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootPoiApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPoiApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(SpringBootPoiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logger.info("\n==============================================\n" +
+				"Application {} is running! Access URL:\n" +
+				"\tLocal:\t\thttp://localhost:{}\n" +
+				"\tExternal:\thttp://{}:{}\n" +
+				"=============================================="
+				,env.getProperty("spring.application.name")
+				,env.getProperty("server.port")
+				,InetAddress.getLocalHost().getHostAddress()
+				,env.getProperty("server.port"));
 	}
 }
